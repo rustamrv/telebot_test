29e42a9d5ab61f38b84c55cc7dca13510a3ce2b4
rystamrv <rystamrv@gmail.com>
2021-01-21 20:33:41 +0200

    add rest c

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, abort
-import json
 from mongoengine import NotUniqueError
 from telebot import TeleBot
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton, Message, InlineKeyboardButton, InlineKeyboardMarkup
+from telebot.types import Message
 from telebot.types import Update
-from .config import TOKEN, WEBHOOK_URI
+from config import TOKEN, WEBHOOK_URI
 from api.app import admin
 from api.resources import RestUsers
 from database.models.models import User
