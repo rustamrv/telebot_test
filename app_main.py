ca488dc4565a4f52a9a1efed5d3a664fb4aad78a
rystamrv <rystamrv@gmail.com>
2021-01-24 20:56:10 +0200

    dd

--- app_main.py
+++ app_main.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 from api.resources.resources import RestUsers
-from bot.main_bot import bot, app
 
-# app = Flask(__name__)
+app = Flask(__name__)
 api = Api(app)
 api.add_resource(RestUsers, '/users')
 
 
+@app.route('/')
 @app.route('/index')
 def index():
     return "Hello world"
