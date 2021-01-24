3901690485a3a2ab3ba6a22b39fd71a2c5c50643
rystamrv <rystamrv@gmail.com>
2021-01-24 16:16:58 +0200

    fixed

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import time
 from bot.config.config import WEBHOOK_URL
 
 
-# bot.remove_webhook()
-# time.sleep(0.5)
-# bot.set_webhook(WEBHOOK_URL, certificate=open('webhook_cert.pem', 'r'))
-# app.run()
-bot.polling()
+bot.remove_webhook()
+time.sleep(0.5)
+bot.set_webhook(WEBHOOK_URL, certificate=open('webhook_cert.pem', 'r'))
+app.run()
+# bot.polling()
